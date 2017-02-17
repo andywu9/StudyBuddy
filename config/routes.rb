f3ecdbc964ac3561db8f23e278e8ef8a01c8d92f
adamsm6 <adamsm6@rpi.edu>
2017-02-16 19:58:23 -0500

    Added Devise Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/homepage'
+
+  devise_for :users
   get 'sign_up/signin'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => "sign_up#signin"
+  root :to => "home#homepage"
 end
