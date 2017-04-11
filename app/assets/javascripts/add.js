1013d19ecc4febe316cd616c78e56fd0b0784722
adamsm6 <adamsm6@rpi.edu>
2017-04-11 09:50:35 -0400

    Cleaned up Javascript and unneccessary files

--- app/assets/javascripts/add.js
+++ app/assets/javascripts/add.js
@@ -1,46 +1,45 @@
 $(function() {
   if($('body').is('.addclass')) {
-  	var myCourses = gon.myCourses;
   	var dict = gon.dict;
 
   	var stateObject = dict;
-    var stateSel = document.getElementById("stateSel"), countySel = document.getElementById("countySel"), citySel = document.getElementById("citySel");
+    var courseSel = document.getElementById("courseSel"), sectionSel = document.getElementById("sectionSel"), finalSel = document.getElementById("finalSel");
     for (var state in stateObject) {
-        stateSel.options[stateSel.options.length] = new Option(state, state);
+        courseSel.options[courseSel.options.length] = new Option(state, state);
     }
-    stateSel.onchange = function () {
-      countySel.length = 1; // remove all options bar first
-      citySel.length = 1; // remove all options bar first
+    courseSel.onchange = function () {
+      sectionSel.length = 1; // remove all options bar first
+      finalSel.length = 1; // remove all options bar first
       if (this.selectedIndex < 1) {
-        countySel.options[0].text = "Please select Department";
-        citySel.options[0].text = "Please select Course";
+        sectionSel.options[0].text = "Please select Department";
+        finalSel.options[0].text = "Please select Course";
         return; // done   
       }  
-      countySel.options[0].text = "Please select Section";
-      for (var county in stateObject[this.value]) {
-          countySel.options[countySel.options.length] = new Option(county, county);
+      sectionSel.options[0].text = "Please select Section";
+      for (var section in stateObject[this.value]) {
+          sectionSel.options[sectionSel.options.length] = new Option(section, section);
       }
-      if (countySel.options.length==2) {
-        countySel.selectedIndex=1;
-        countySel.onchange();
+      if (sectionSel.options.length==2) {
+        sectionSel.selectedIndex=1;
+        sectionSel.onchange();
       }  
     }
-    stateSel.onchange(); // reset in case page is reloaded
-    countySel.onchange = function () {
-        citySel.length = 1; // remove all options bar first
+    courseSel.onchange(); // reset in case page is reloaded
+    sectionSel.onchange = function () {
+        finalSel.length = 1; // remove all options bar first
         if (this.selectedIndex < 1) {
-          citySel.options[0].text = "Please select course first";
+          finalSel.options[0].text = "Please select course first";
           return; // done   
         }  
-        citySel.options[0].text = "Please select Section";
+        finalSel.options[0].text = "Please select Section";
         
-        var cities = stateObject[stateSel.value][this.value];
+        var cities = stateObject[courseSel.value][this.value];
         for (var i = 0; i < cities.length; i++) {
-            citySel.options[citySel.options.length] = new Option(cities[i], cities[i]);
+            finalSel.options[finalSel.options.length] = new Option(cities[i], cities[i]);
         }
-        if (citySel.options.length==2) {
-          citySel.selectedIndex=1;
-          citySel.onchange();
+        if (finalSel.options.length==2) {
+          finalSel.selectedIndex=1;
+          finalSel.onchange();
         }  
         
     }
