dcfcd1b3c9855a30c31086b978e18a2ef85cc863
Andy <wua6@rpi.edu>
2017-04-17 21:18:37 -0400

    Updated the calendar to reflect changes after removing courses.

--- app/controllers/remove_class_controller.rb
+++ app/controllers/remove_class_controller.rb
@@ -1,16 +1,14 @@
 class RemoveClassController < ApplicationController
 
 	def remove
+		# Stores the current user's courses used to populate dropdown field
 		gon.keys = Username.uniq.pluck(:course_number).sort
-
-		gon.keys.each do |course|
-			puts course
-		end
 	end		
 
-	
+	# Used to remove a course from my courses and updates calendar to reflect changes
 	def removeClass
 		Username.destroy_all(:course_number => params[:optone])
+		Event.destroy_all(:title => params[:optone])
 	end
 
 
