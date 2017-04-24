61c207175eb86496729bd6df76e010f40b9a547b
jtorella <torella.jeremy@gmail.com>
2017-04-24 01:24:58 -0400

    Added plan table to create plan page and functionality to add classes to the table.

--- app/assets/javascripts/create_plan.js
+++ app/assets/javascripts/create_plan.js
@@ -1,13 +1,18 @@
 $(function() {
 	if($('body').is('.createPlanPage')) {
 
-		var plans = gon.keys;
+		var plans = gon.planDict;
 		//populate dropdown with plans
-		planList = document.getElementById("editPlan")
+		var planList = document.getElementById("planSelect")
 		for (var title in plans) {
 			 planList.options[planList.options.length] = new Option(title, title);
 		}
 
+        var classes = gon.classDict
+        var classList = document.getElementById("newClassSelect");
+        for (var c in classes) {
+             classList.options[classList.options.length] = new Option(c, c);
+        }
 
 		//drop down reveals html
 		$(document).on('change', '#newOrEditPlan', function() {
@@ -25,5 +30,28 @@ $(function() {
         $(document).ready(function(){
             $('.planSelect').trigger('change');
         });
+
+
+
+
+
+        var addCtoPBtn = document.getElementById("addCToP");
+
+        addCtoPBtn.onclick = addToPlan
+
+        function addToPlan() {
+            var planTable = document.getElementById("planTable");
+
+            var newRow = document.createElement('div');
+
+            newRow.className = 'tableRow'
+
+            classList = document.getElementById("newClassSelect");
+            var inputBox = document.getElementById('newDurationInput');
+            newRow.innerHTML = '<div class="planClass cell">' + classList.options[classList.selectedIndex].text + '</div><div class="planDur cell">' + inputBox.value + '</div>';
+
+
+            planTable.append(newRow);
+        }
 	}
 });
--- app/controllers/create_plan_controller.rb
+++ app/controllers/create_plan_controller.rb
@@ -1,8 +1,40 @@
 class CreatePlanController < ApplicationController
   def index
-  	gon.keys = StudyPlan.uniq.pluck(:planName).sort
 
+  	#plan variables
+  	gon.keys = StudyPlan.uniq.pluck(:planName).sort
+	gon.planDict = Hash.new
+
+  	gon.keys.each do |plan|
+  		gon.planSub_Dict = Hash.new
+  		gon.plans =  Course.where(:department_code => plan).pluck(:course_name).sort
+
+  		gon.plans.each do |classy|  # store all sections for the course and dpt
+  			gon.sections=  Period.where(" department_code = ? AND course_name = ?", plan, classy).pluck(:section_name).uniq
+  			gon.planSub_Dict[classy] = gon.sections
+  		end
+  		gon.planDict[plan] = gon.planSub_Dict  # set the outer dictionaries value to be the sub dictionary
+  	end
+
+  	#class variables
+  	gon.classes = Username.uniq.pluck(:course_number).sort
+  	gon.classDict = Hash.new
+
+  	gon.classes.each do |c|
+  		gon.classSub_Dict = Hash.new
+  		gon.classList =  Course.where(:department_code => c).pluck(:course_name).sort
+
+  		gon.classList.each do |cl|  # store all sections for the course and dpt
+  			gon.classSections =  Period.where(" department_code = ? AND course_name = ?", c, cl).pluck(:section_name).uniq
+  			gon.classSub_Dict[cl] = gon.classSections
+  		end
+  		gon.classDict[c] = gon.classSub_Dict  # set the outer dictionaries value to be the sub dictionary
   	
+	end
+  end
+
+  def addPlan
+		#TODO: FILL THIS OUT
 
   end
 end
